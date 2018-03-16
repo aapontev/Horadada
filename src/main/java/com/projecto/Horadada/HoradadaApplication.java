21c46dec6186d424c06f842eb1108ad506cbb245
alexander aponte villena <alexanderaponte47@gmail.com>
2018-03-16 17:56:07 -0500

    1245

---
+++ src/main/java/com/projecto/Horadada/HoradadaApplication.java
@@ -0,0 +1,12 @@
+package com.projecto.Horadada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoradadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoradadaApplication.class, args);
+	}
+}
