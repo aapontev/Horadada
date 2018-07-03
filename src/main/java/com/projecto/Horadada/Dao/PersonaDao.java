3e36fd314ed58ae95cb5b3a3b1e8683806b20cea
alexander <alexander@192.168.1.11>
2018-07-02 20:34:27 -0500

    fd

--- src/main/java/com/projecto/Horadada/Dao/PersonaDao.java
+++ src/main/java/com/projecto/Horadada/Dao/PersonaDao.java
@@ -1,16 +1,13 @@
 package com.projecto.Horadada.Dao;
 
-
 import java.io.Serializable;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.projecto.Horadada.Entity.Persona;
-
 
 @Repository("personaDao")
 public interface PersonaDao extends CrudRepository<Persona, Serializable>{
@@ -23,5 +20,4 @@ public interface PersonaDao extends CrudRepository<Persona, Serializable>{
 
 	@Query(value="update persona set tipopersona = :tipo where idpersona = :id" ,nativeQuery=true)
 	public abstract Persona updatetipopersona(@Param("tipo")int tipo,@Param("id")int id);
-
 }
