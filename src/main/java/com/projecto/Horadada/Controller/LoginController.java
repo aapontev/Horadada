d925a339c860002df9ee8ad586e601188d4a693b
aaponte <aaponte@IQPC-016.quasys.com>
2018-09-04 18:30:19 -0500

    oiu

--- src/main/java/com/projecto/Horadada/Controller/LoginController.java
+++ src/main/java/com/projecto/Horadada/Controller/LoginController.java
@@ -1,11 +1,12 @@
 package com.projecto.Horadada.Controller;
 
+
+import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.projecto.Horadada.Model.Usuarios;
 
@@ -18,21 +19,28 @@ public class LoginController {
 	}
 
 	@GetMapping("/login")
-	public String showLoginForm(Model model, @RequestParam(name = "error", required = false) String error,
-			@RequestParam(name = "logout", required = false) String logout) {
-		model.addAttribute("error", error);
+	public String showLoginForm(Model model, Principal principal, @RequestParam(name = "error", required = false) String error,
+			@RequestParam(name = "logout", required = false) String logout, RedirectAttributes flas) {
+		
 		model.addAttribute("logout", logout);
 		model.addAttribute("credencialUsuario", new Usuarios());
+		if (principal != null) {
+			flas.addFlashAttribute("info", "ya inicio sesion anteriormente");
+		}
+		if (error != null) {
+			model.addAttribute("error", "tiene problemas de autentificacion por favor vuelva a intentarlo");
+		}
+			
 		return "login";
 	}
 
-	@PostMapping("/loginCheck")
+	/*@PostMapping("/loginCheck")
 	public String loginCheck(@ModelAttribute(name = "credencialUsuario") Usuarios usuario) {
 		if (usuario.getUsuario().equals("usu") && usuario.getPassword().equals("con")) {
 			return "index";
 		}
 		return "redirect:/login?error";
-	}
+	}*/
 
 	@GetMapping("/index")
 	public String index() {
---
+++ src/main/java/com/projecto/Horadada/SpringSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.projecto.Horadada;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/css/**","/js/**","/imgs/**","/fonts/**").permitAll()
+		.antMatchers("/consulta/**").hasAnyRole("USER")
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/login")
+		.permitAll()
+		.and()
+		.logout().permitAll();
+	}
+
+	@Autowired
+	public void ConfigurerGlobal(AuthenticationManagerBuilder build)throws Exception {
+		PasswordEncoder pass = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		UserBuilder users = User.builder().passwordEncoder(pass::encode);
+		
+		build.inMemoryAuthentication()
+		.withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
+		.withUser(users.username("alex").password("12345").roles("USER"));
+	}
+}
